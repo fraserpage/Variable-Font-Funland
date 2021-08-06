3eb87a547f6611df0fe072442774110694dc72b4
fraserpage <fraserpage@gmail.com>
2021-08-05 23:32:16 -0400

    redi auth

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken')
 const SECRET = process.env.SECRET
 
-module.exports = function(req,res, next){
-  let token = req.get('Authorization') || req.query.token || req.body.token
-  if (token){
-    token = token.replace('Bearer ','')
-    jwt.verify(token, SECRET, (err, decoded)=>{
-      if (err) {
-        next(err)
-      }
-      else{
-        req.user = decoded.user
-        next()
-      }
-    })
+module.exports = function(req, res, next) {
+  // Check for the token being sent in a header or as a query parameter
+  let token = req.get('Authorization') || req.query.token;
+  if (token) {
+    // Remove the 'Bearer ' if it was included in the token header
+    token = token.replace('Bearer ', '');
+    // Check if token is valid and not expired
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+      // If valid token, req.user will be the user obj in the token's payload
+      // If invalid token, req.user will be null
+      req.user = err ? null : decoded.user;    
+      return next();
+    });
+  } else {
+    req.user = null;
+    return next();
   }
-  else{
-    next()
-  }
-}
+};
