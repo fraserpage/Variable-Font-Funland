e693f913f6c817405541d8f03fda241164ac6ed3
fraserpage <fraserpage@gmail.com>
2021-08-05 23:33:25 -0400

    build out word art utils

--- src/utils/wordArtUtils.js
+++ src/utils/wordArtUtils.js
@@ -1,13 +1,17 @@
 import { post, get } from "./fetchUtils";
 
 export async function createWordArt(data){
+  delete data['_id']
   let wordArt = await post('/api/word-art', data)
-  console.log(wordArt)
+  return wordArt
+}
+
+export async function getOneWordArt(id){
+  let wordArt = await get(`/api/word-art/${id}`)
   return wordArt
 }
 
 export async function getAllWordArt(){
   let wordArt = await get('/api/word-art')
-  console.log(wordArt)
   return wordArt
 }
