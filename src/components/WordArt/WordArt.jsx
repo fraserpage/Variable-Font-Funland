795a2d65a0e5e1fc611687a67a3a63123a34e4c3
fraserpage <fraserpage@gmail.com>
2021-08-05 23:31:11 -0400

    add WordArt component

--- src/components/WordArt/WordArt.jsx
+++ src/components/WordArt/WordArt.jsx
@@ -1,7 +1,75 @@
+const wordArtStyle = (props) => {
+  const styleObj = {}
+  const stylePoints = props.animations[0].stylePoints
+  const numStylePoints = stylePoints.length
+  if (numStylePoints === 1){
+    const stylePoint = props.animations[0].stylePoints[0]
+    styleObj['fontFamily'] = stylePoint.font
+    styleObj['fontSize'] = stylePoint.fontSize+'em'
+    styleObj['lineHeight'] = stylePoint.lineHeight+'em'
+    styleObj['letterSpacing'] = stylePoint.letterSpacing+'em'
+    styleObj['fontVariationSettings'] = stylePoint.vars.map(v=>`"${v.var}" ${v.val}`).join(", ")
+  }
+  else{
+    styleObj['lineHeight'] = 0
+  }
+  styleObj['whiteSpace'] = 'pre-line';
+  styleObj['fontFamily'] = props.font
+  return styleObj
+}
+
+const wordArtStyleWithEase = (stylePoints,currentPoint,ease) => {
+  const styleObj = {}
+  styleObj['fontSize'] = easeProp(stylePoints,currentPoint,'fontSize',ease) + 'em'
+  styleObj['lineHeight'] = easeProp(stylePoints,currentPoint,'lineHeight',ease) + 'em'
+  styleObj['letterSpacing'] = easeProp(stylePoints,currentPoint,'letterSpacing',ease) +'em'
+  styleObj['fontVariationSettings'] = stylePoints[currentPoint].vars.map((v,i)=>`"${v.var}" ${easeFontVar(stylePoints,currentPoint,ease,i)}`).join(", ")
+  return styleObj
+}
+
+function easeFontVar(stylePoints,currentPoint,ease,i){
+  let from = stylePoints[currentPoint - 1].vars[i].val 
+  let to = stylePoints[currentPoint].vars[i].val 
+  return from + ((to - from) * ease)
+}
+
+function easeProp(stylePoints, currentPoint, prop, ease){
+  const from = stylePoints[currentPoint-1][prop]
+  const to = stylePoints[currentPoint][prop]
+  return from + ((to - from) * ease)
+}
+
+function wordArtCharStyle(props,charPos){
+  let styleObj = {}
+  const stylePoints = props.animations[0].stylePoints
+  const numStylePoints = stylePoints.length
+  if (numStylePoints > 1){
+    const textLength = props.text.length -1
+    const currentPos = charPos / textLength
+    let currentPoint = stylePoints.length - 1
+    for (let i = 0; i < stylePoints.length; i++){
+      if (currentPos < stylePoints[i].point){
+        currentPoint = i
+        break
+      }
+    }
+    let posInCurve = (currentPos - stylePoints[currentPoint-1].point)/(stylePoints[currentPoint].point - stylePoints[currentPoint-1].point)
+    let ease = props.animations[0].easing === 'linear' ? posInCurve : easeInOutQuad(posInCurve)
+    styleObj = wordArtStyleWithEase(stylePoints,currentPoint,ease)
+  }
+  return styleObj
+}
+
+function easeInOutQuad(x) {
+  return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+}
+
 export default function WordArt(props){
   return(
-    <a href={`/playground/${props._id}`}>
-      {props.text}
-    </a>
+    <div className="word-art" style={wordArtStyle(props)}>
+      {props.text.split('').map((char,i)=>(
+        <span key={i} style={wordArtCharStyle(props,i)}>{char}</span>
+      ))}
+    </div>
   )
 }
---
+++ src/components/WordArtControls/WordArtControls.jsx
@@ -0,0 +1,139 @@
+import _ from 'lodash';
+import styled from 'styled-components';
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css';
+import Select from 'react-select'
+import FontOptions from '../FontOptions/FontOptions';
+
+const List = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  max-width: 15rem;
+`
+const handleSlider = (e,i, props) => {
+  const animation = props.currentlyEditing.animation
+  const stylePoint = props.currentlyEditing.stylePoint
+  const animations = props.animations
+
+  if (typeof i === 'string'){
+    animations[animation].stylePoints[stylePoint][i] = e
+  }
+  else{
+    animations[animation].stylePoints[stylePoint].vars[i].val = e
+  }
+  props.setPlaygroundState({ animations })
+}
+
+const handleAddStylePoint = (props) => {
+  const animation = props.currentlyEditing.animation
+  const animations = props.animations
+  // save the new length of the stylePoints list
+  const length = animations[animation].stylePoints.push(
+    // push a copy of stylePoint 0 to stylePoints
+    _.cloneDeep(animations[animation].stylePoints[0])
+  )
+  animations[animation].stylePoints[length - 1].point = 1
+  const currentlyEditing = props.currentlyEditing
+  currentlyEditing.stylePoint = length - 1
+  props.setPlaygroundState({ 
+    animations,
+    currentlyEditing
+  })
+}
+
+const handleChangeEditPoint = (point, props) => {
+  const currentlyEditing = props.currentlyEditing
+  currentlyEditing.stylePoint = point
+  props.setPlaygroundState({ 
+    currentlyEditing
+  })
+}
+
+const handleSelect = (e, prop, props) =>{
+  const animation = props.currentlyEditing.animation
+  const animations = props.animations
+  animations[animation][prop] = e.value
+  props.setPlaygroundState({ 
+    animations
+  })
+}
+
+export default function WordArtControls(props){
+
+  const animation = props.currentlyEditing.animation
+  const stylePoint = props.currentlyEditing.stylePoint
+  const currentStyle = props.animations[animation].stylePoints[stylePoint]
+
+  return(
+    <div>
+
+      <div>
+        <button onClick={()=>handleAddStylePoint(props)}>Add style point</button> 
+      </div>
+      
+      {props.animations[animation].stylePoints.map((s,i) =>(
+        <p onClick={()=>handleChangeEditPoint(i,props)} key={i}>
+          { i === props.currentlyEditing.stylePoint && 'currently editing point '}
+          {s.point}
+        </p>
+      ))}
+
+      <List id='controls'>
+        <li>
+          <FontOptions {...props}/>
+        </li>
+        <li>
+          <label htmlFor='font-size'>Size</label>
+          <Slider 
+            id='font-size' 
+            value={currentStyle.fontSize} 
+            min={0.2} max={2} step={0.05}
+            onChange={(e)=>handleSlider(e,'fontSize',props)} 
+          />
+        </li>
+        <li>
+          <label htmlFor='letter-spacing'>Letter Spacing</label>
+          <Slider 
+            id='letter-spacing' 
+            value={currentStyle.letterSpacing}
+            min={-0.2} max={0.5} marks={{0:''}} step={0.01}
+            onChange={(e)=>handleSlider(e,'letterSpacing',props)} 
+          />
+        </li>
+        <li>
+          <label htmlFor='line-height'>Line Height</label>
+          <Slider 
+            id='line-height'
+            value={currentStyle.lineHeight} 
+            min={0.1} max={3.5} step={0.01}
+            onChange={(e)=>handleSlider(e,'lineHeight',props)}  
+          />
+        </li>
+        {currentStyle.vars.map((v,i)=>(
+          <li key={i} >
+            <label htmlFor={v.var}>{v.var}</label>
+            <Slider 
+              id={v.var} 
+              value={currentStyle.vars[i].val}
+              min={v.min} max={v.max}  step={v.step}
+              onChange={(e)=>handleSlider(e,i,props)} 
+            />
+          </li>
+        ))}
+        <li>
+          <label id='easing-label'>Easing</label>
+          <Select 
+            aria-labelledby='easing-label'
+            defaultValue={{ value: 'linear', label: 'Linear' }}  
+            options={[
+              { value: 'linear', label: 'Linear' },
+              { value: 'easeInOutQuad', label: 'EaseInOut' }
+            ]} 
+            onChange={e=>handleSelect(e,'easing',props)}
+          />
+        </li>
+      </List>
+    </div>
+  )
+}
