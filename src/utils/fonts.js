de6902fb0bd86eea0e84f96a0e1feb10f9f4fcbd
fraserpage <fraserpage@gmail.com>
2021-08-05 23:32:39 -0400

    add fonts json

---
+++ src/utils/fonts.js
@@ -0,0 +1,107 @@
+const fonts = [
+  {
+    font: 'Recursive',
+    vars: [
+      { var: 'slnt',	val: 0,		min: -15,	max: 0,		step: 1 },
+      { var: 'wght',	val: 400,	min: 300,	max: 1000,step: 1 },
+      { var: 'CASL',	val: 0,		min: 0,		max: 1,		step: 0.01 },
+      { var: 'CRSV',	val: 0.5,	min: 0,		max: 1,		step: 0.1 },
+      { var: 'MONO',	val: 0,		min: 0,		max: 1,		step: 0.01 }
+    ]
+  },
+  {
+    font: 'Archivo',
+    vars: [
+      // { var: 'ital',	val: 0,		min: 0,		max: 1,		step: 1 },
+      { var: 'wdth',	val: 100,	min: 62,	max: 125,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 100,	max: 900,	step: 1 }
+    ]
+  },
+  {
+    font: 'Cabin',
+    vars: [
+      // { var: 'ital',	val: 0,   min: 0,		max: 1,		step: 1 },
+      { var: 'wdth',	val: 100,	min: 75,	max: 100,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 400,	max: 700,	step: 1 },
+    ]
+  },
+  {
+    font: 'Encode Sans',
+    vars: [
+      { var: 'wdth',	val: 100,	min: 75,	max: 125,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 100,	max: 900,	step: 1 },
+    ]
+  },
+  {
+    font: 'Fraunces',
+    vars: [
+      // { var: 'ital',	val: 0,		min: 0,		max: 1,		step: 1 },
+      { var: 'opsz',	val: 14,	min: 9,		max: 144,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 100,	max: 900,	step: 1 },
+      { var: 'SOFT',	val: 0,		min: 0,		max: 100,	step: 0.1 },
+    ]
+  },
+  {
+    font: 'Georama',
+    vars: [
+      // { var: 'ital',	val: 0,		min: 0,		 max: 1,	 step: 1 },
+      { var: 'wdth',	val: 100,	min: 62.5, max: 150, step: 0.1 },
+      { var: 'wght',	val: 400,	min: 100,	 max: 900, step: 1 }
+    ]
+  },
+  {
+    font: 'Inconsolata',
+    vars: [
+      { var: 'wdth',	val: 100,	min: 50,	max: 200,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 200,	max: 900,	step: 1 }
+    ]
+  },
+  {
+    font: 'Inter',
+    vars: [
+      { var: 'slnt',	val: 0,		min: -10,	max: 0,	  step: 1 },
+      { var: 'wght',	val: 400,	min: 100,	max: 900, step: 1 },
+    ]
+  },
+  {
+    font: 'Literata',
+    vars: [
+      // { var: 'ital',	val: 0,		min: 0,		max: 1,		step: 1 },
+      { var: 'opsz',	val: 14,	min: 7,		max: 72,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 200,	max: 900,	step: 1 },
+    ]
+  },
+  {
+    font: 'Saira',
+    vars: [
+      // { var: 'ital',	val: 0,		min: 0,		max: 1,		step: 1 },
+      { var: 'wdth',	val: 100,	min: 50,	max: 125,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 100,	max: 900,	step: 1 },
+    ]
+  },
+  {
+    font: 'Tourney',
+    vars: [
+      // { var: 'ital',	val: 0,		min: 0,		max: 1,		step: 1 },
+      { var: 'wdth',	val: 100,	min: 75,	max: 125,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 100,	max: 900,	step: 1 },
+    ]
+  },
+  {
+    font: 'Trispace',
+    vars: [
+      { var: 'wdth',	val: 100,	min: 75,	max: 125,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 100,	max: 800,	step: 1 },
+    ]
+  },
+  {
+    font: 'Truculenta',
+    vars: [
+      { var: 'opsz',	val: 14,	min: 12,	max: 72,	step: 0.1 },
+      { var: 'wdth',	val: 100,	min: 75,	max: 125,	step: 0.1 },
+      { var: 'wght',	val: 400,	min: 100,	max: 900,	step: 1 },
+    ]
+  }
+]
+
+export default fonts
